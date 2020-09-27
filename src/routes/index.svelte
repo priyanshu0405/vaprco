d8867bd757108090629113cf7b18b6b2e0c2c42d
therealansh <tyagiansh23@hotmail.com>
2020-09-28 03:04:49 +0530

    added glitch effect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,80 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<style type="text/scss">
+  :global(body) {
+    background-color: black;
+  }
+  .glitch {
+    font: {
+      size: 8.125em;
+      family: "Raleway", sans-serif;
+      weight: 700;
+    }
+    text-decoration: none;
+    text-transform: uppercase;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    color: white;
+    letter-spacing: 5px;
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    &:before,
+    &:after {
+      display: block;
+      content: attr(data-glitch);
+      text-transform: uppercase;
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      opacity: 0.8;
+    }
+    &:after {
+      color: #f0f;
+      z-index: -2;
+    }
+    &:before {
+      color: #0ff;
+      z-index: -1;
+    }
+    &:hover {
+      &:before {
+        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both 5;
+      }
+      &:after {
+        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both
+          5;
+      }
+    }
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  @media only screen and (max-width: 400px) {
+    .glitch {
+      font-size: 3em;
+    }
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @keyframes glitch {
+    0% {
+      transform: translate(0);
+    }
+    20% {
+      transform: translate(-5px, 5px);
+    }
+    40% {
+      transform: translate(-5px, -5px);
+    }
+    60% {
+      transform: translate(5px, 5px);
+    }
+    80% {
+      transform: translate(5px, -5px);
+    }
+    to {
+      transform: translate(0);
+    }
+  }
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="glitch" data-glitch="VAPR">VAPR</div>
