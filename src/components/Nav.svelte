fed5d1fa1a21bad907a50ac80ede6a1156598a01
Mehul-12 <58396824+Mehul-12@users.noreply.github.com>
2020-10-14 12:28:16 +0530

    changed blog to team (#22)
    * added icons in footer
    
    * changed blog to team

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -159,6 +159,6 @@ nav.fill ul li a:hover:after {
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Blog</a></li>
+		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Team</a></li>
 	</ul>
 </nav>
