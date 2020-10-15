22f08f74a4219848e20e57b372c38158d004e897
Arpit Agarwal <58038078+aarpit1010@users.noreply.github.com>
2020-10-15 16:05:46 +0530

    Added Title in Team Page

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,34 +1,81 @@
-<script context="module">
-	export function preload() {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
-</script>
+<style type="text/scss">
+  :global(body) {
+    background-color: black;
+  }
+  .glitch {
+    font: {
+      size: 4.8em;
+      family: "Open sans", sans-serif;
+      weight: 700;
+    }
+    text-decoration: none;
+    text-transform: uppercase;
+		text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    color: white;
+    letter-spacing: 5px;
 
-<script>
-	export let posts;
-</script>
+    &:before,
+    &:after {
+      display: block;
+      content: attr(data-glitch);
+      text-transform: uppercase;
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      opacity: 0.8;
+    }
+    &:after {
+      color: #f0f;
+      z-index: -2;
+    }
+    &:before {
+      color: #0ff;
+      z-index: -1;
+    }
+    &:hover {
+      &:before {
+        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both 5;
+      }
+      &:after {
+        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both
+          5;
+      }
+    }
+  }
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
+  @media only screen and (max-width: 420px) {
+    .glitch {
+      font-size: 3em;
+    }
+  }
+
+  @keyframes glitch {
+    0% {
+      transform: translate(0);
+    }
+    20% {
+      transform: translate(-5px, 5px);
+    }
+    40% {
+      transform: translate(-5px, -5px);
+    }
+    60% {
+      transform: translate(5px, 5px);
+    }
+    80% {
+      transform: translate(5px, -5px);
+    }
+    to {
+      transform: translate(0);
+    }
+  }
 </style>
 
-<svelte:head>
-	<title>Blog</title>
-</svelte:head>
-
-<h1>Recent posts</h1>
-
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
-	{/each}
-</ul>
+<div class="glitch" data-glitch="our team">our team</div>
