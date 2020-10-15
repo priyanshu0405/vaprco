075f76621f28d74fdb6d6ff91bb960caf07e4c6b
Arpit Agarwal <58038078+aarpit1010@users.noreply.github.com>
2020-10-15 16:30:16 +0530

    Update index.svelte
    removed glich effect and text appears in a single line

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -4,76 +4,24 @@
   }
   .glitch {
     font: {
-      size: 4.8em;
+      size: calc(4vw + 4vh);
       family: "Open sans", sans-serif;
       weight: 700;
     }
-    text-decoration: none;
     text-transform: uppercase;
 		text-align: center;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    margin: 0;
     color: white;
-    letter-spacing: 5px;
+    letter-spacing: 4px;
+    white-space: nowrap;
 
-    &:before,
-    &:after {
-      display: block;
-      content: attr(data-glitch);
-      text-transform: uppercase;
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 100%;
-      opacity: 0.8;
-    }
-    &:after {
-      color: #f0f;
-      z-index: -2;
-    }
-    &:before {
-      color: #0ff;
-      z-index: -1;
-    }
-    &:hover {
-      &:before {
-        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) both 5;
+    @media only screen and (max-width: 200px) {
+      .glitch {
+        font-size: 1em;
       }
-      &:after {
-        animation: glitch 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both
-          5;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 420px) {
-    .glitch {
-      font-size: 3em;
-    }
-  }
-
-  @keyframes glitch {
-    0% {
-      transform: translate(0);
-    }
-    20% {
-      transform: translate(-5px, 5px);
-    }
-    40% {
-      transform: translate(-5px, -5px);
-    }
-    60% {
-      transform: translate(5px, 5px);
-    }
-    80% {
-      transform: translate(5px, -5px);
-    }
-    to {
-      transform: translate(0);
     }
   }
 </style>
